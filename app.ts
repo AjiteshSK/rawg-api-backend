3d2164ca359c332dcee568a70482ff33c58e7704
ajiteshk <ajiteshkahlon51@gmail.com>
2022-06-27 12:51:36 +0530

    static file

--- app.ts
+++ app.ts
@@ -13,6 +13,8 @@ app.use(cors());
 
 app.use(router);
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('*', (req, res) => {
   console.log('send file');
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
